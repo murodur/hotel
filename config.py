8440409a5ae9c327c75792d7e8b85066ee76eee5
murodur <32630241+murodur@users.noreply.github.com>
2024-01-28 22:18:14 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,1 @@
-host = "185.105.110.5"
-port = 3306
-user = "p532346_p532346"
-password = "xLDfn3HgCT"
-db_name = "p532346_nj"
-
-
-# localhost
-# host = "localhost"
-# port = 3306
-# user = "root"
-# password = ""
-# db_name = "hotel"
 
